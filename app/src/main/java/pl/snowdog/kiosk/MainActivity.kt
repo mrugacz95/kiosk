73cf889a2611f386810e10c4ee4eff4fc530eb1a
Marcin Mrugas <marcin.mrugas@snow.dog>
2018-03-22 15:51:44 +0100

    Added LockTask mode

--- app/src/main/java/pl/snowdog/kiosk/MainActivity.kt
+++ app/src/main/java/pl/snowdog/kiosk/MainActivity.kt
@@ -1,28 +1,150 @@
 package pl.snowdog.kiosk
 
 import android.app.admin.DevicePolicyManager
+import android.app.admin.SystemUpdatePolicy
 import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
+import android.os.UserManager
+import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-
 
     private lateinit var mAdminComponentName: ComponentName
     private lateinit var mDevicePolicyManager: DevicePolicyManager
 
+    companion object {
+        const val LOCK_ACTIVITY_KEY = "pl.snowdog.kiosk.MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mAdminComponentName = MyDeviceAdminReceiver.getComponentName(this)
         mDevicePolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
 
+        var isAdmin = false
         if (mDevicePolicyManager.isDeviceOwnerApp(packageName)) {
             Toast.makeText(applicationContext, R.string.device_owner, Toast.LENGTH_SHORT).show()
+            isAdmin = true
         } else {
             Toast.makeText(applicationContext, R.string.not_device_owner, Toast.LENGTH_SHORT).show()
+        }
+        btStartLockTask.setOnClickListener {
+            setKioskPolicies(true, isAdmin)
+        }
+        btStopLockTask.setOnClickListener {
+
+            setKioskPolicies(false, isAdmin)
+            val intent = Intent(applicationContext, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
+            intent.putExtra(LOCK_ACTIVITY_KEY, false)
+            startActivity(intent)
+        }
+
+
+    }
+
+    private fun setKioskPolicies(enable: Boolean, isAdmin: Boolean) {
+        if (isAdmin) {
+            setRestrictions(enable)
+            enableStayOnWhilePluggedIn(enable)
+            setUpdatePolicy(enable)
+            setAsHomeApp(enable)
+            setKeyGuardEnabled(enable)
+        }
+        setLockTask(enable, isAdmin)
+        setImmersiveMode(enable)
+    }
+
+    // region restrictions
+    private fun setRestrictions(disallow: Boolean) {
+        setUserRestriction(UserManager.DISALLOW_SAFE_BOOT, disallow)
+        setUserRestriction(UserManager.DISALLOW_FACTORY_RESET, disallow)
+        setUserRestriction(UserManager.DISALLOW_ADD_USER, disallow)
+        setUserRestriction(UserManager.DISALLOW_MOUNT_PHYSICAL_MEDIA, disallow)
+        setUserRestriction(UserManager.DISALLOW_ADJUST_VOLUME, disallow)
+    }
+
+    private fun setUserRestriction(restriction: String, disallow: Boolean) = if (disallow) {
+        mDevicePolicyManager.addUserRestriction(mAdminComponentName, restriction)
+    } else {
+        mDevicePolicyManager.clearUserRestriction(mAdminComponentName, restriction)
+    }
+    // endregion
+
+    private fun enableStayOnWhilePluggedIn(active: Boolean) = if (active) {
+        mDevicePolicyManager.setGlobalSetting(mAdminComponentName,
+                Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
+                Integer.toString(BatteryManager.BATTERY_PLUGGED_AC
+                        or BatteryManager.BATTERY_PLUGGED_USB
+                        or BatteryManager.BATTERY_PLUGGED_WIRELESS))
+    } else {
+        mDevicePolicyManager.setGlobalSetting(mAdminComponentName, Settings.Global.STAY_ON_WHILE_PLUGGED_IN, "0")
+    }
+
+    private fun setLockTask(start: Boolean, isAdmin: Boolean) {
+        if (isAdmin) {
+            mDevicePolicyManager.setLockTaskPackages(mAdminComponentName, if (start) arrayOf(packageName) else arrayOf())
+        }
+        if (start) {
+            startLockTask()
+        } else {
+            stopLockTask()
+        }
+    }
+
+    private fun setUpdatePolicy(enable: Boolean) {
+        if (enable) {
+            mDevicePolicyManager.setSystemUpdatePolicy(mAdminComponentName,
+                    SystemUpdatePolicy.createWindowedInstallPolicy(60, 120))
+        } else {
+            mDevicePolicyManager.setSystemUpdatePolicy(mAdminComponentName, null)
+        }
+    }
+
+    private fun setAsHomeApp(enable: Boolean) {
+        if (enable) {
+            val intentFilter = IntentFilter(Intent.ACTION_MAIN).apply {
+                addCategory(Intent.CATEGORY_HOME)
+                addCategory(Intent.CATEGORY_DEFAULT)
+            }
+            mDevicePolicyManager.addPersistentPreferredActivity(
+                    mAdminComponentName, intentFilter, ComponentName(packageName, MainActivity::class.java.name))
+        } else {
+            mDevicePolicyManager.clearPackagePersistentPreferredActivities(
+                    mAdminComponentName, packageName)
+        }
+    }
+
+    private fun setKeyGuardEnabled(enable: Boolean) {
+        mDevicePolicyManager.setKeyguardDisabled(mAdminComponentName, !enable)
+    }
+
+    private fun setImmersiveMode(enable: Boolean) {
+        if (enable) {
+            val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            window.decorView.systemUiVisibility = flags
+        } else {
+            val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            window.decorView.systemUiVisibility = flags
         }
     }
 }
--- app/src/main/java/pl/snowdog/kiosk/MyDeviceAdminReceiver.kt
+++ app/src/main/java/pl/snowdog/kiosk/MyDeviceAdminReceiver.kt
@@ -3,11 +3,25 @@ package pl.snowdog.kiosk
 import android.app.admin.DeviceAdminReceiver
 import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 
 class MyDeviceAdminReceiver : DeviceAdminReceiver() {
     companion object {
         fun getComponentName(context: Context): ComponentName {
             return ComponentName(context.applicationContext, MyDeviceAdminReceiver::class.java)
         }
+
+        private val TAG = MyDeviceAdminReceiver::class.java.simpleName
+    }
+
+    override fun onLockTaskModeEntering(context: Context?, intent: Intent?, pkg: String?) {
+        super.onLockTaskModeEntering(context, intent, pkg)
+        Log.d(TAG, "onLockTaskModeEntering")
+    }
+
+    override fun onLockTaskModeExiting(context: Context?, intent: Intent?) {
+        super.onLockTaskModeExiting(context, intent)
+        Log.d(TAG, "onLockTaskModeExiting")
     }
 }
